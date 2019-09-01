1690b046f6355824a655f1253fb691b8fd8479a2
Anatols Baymaganov <anatols.baymaganov@gmail.com>
2019-09-01 21:50:25 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 require "faker"
 require "ruby-progressbar"
 
-user_logins = (1..100).map { Faker::Name.unique.last_name }
+user_logins = (1..100).map { Faker::Internet.unique.username }
 author_ips = (1..50).map { Faker::Internet.unique.ip_v4_address }
 posts_count = 200_000
 progress = ProgressBar.create(total: posts_count, format: "%a %b\u{15E7}%i %p%% %t",
@@ -12,6 +12,12 @@ progress = ProgressBar.create(total: posts_count, format: "%a %b\u{15E7}%i %p%% 
 posts_count.times do
   post = Creators::Post.call(login: user_logins.sample, title: Faker::Lorem.word,
                              text: Faker::Lorem.paragraph, author_ip: author_ips.sample)
-  rand(6).times { Creators::Score.call(post_id: post.id, value: rand(1..5)) }
+  raise post.to_h unless post.is_a?(Post)
+
+  rand(6).times do
+    res = Creators::Score.call(post_id: post.id, value: rand(1..5))
+    raise res.to_h unless res.is_a?(Score)
+  end
+
   progress.increment
 end
